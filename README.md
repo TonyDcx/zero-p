9d124b48db6cdf5e52d0cc93c9db4592f4ea8876
TonDcx <official.tony.one@gmail.com>
2021-04-09 02:28:22 +0300

    New Design Changes

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # zero-p
 
-Zero-P Version 1.0.0
+--Zero-P Version 1.0.0 (9.04.2021)--
 
 Added the files .
 
 Added some pc-only design with css .
+
+Added some design / font / variables and rules / infos for variables .
+
+Don't forget to check my js file and see the multi-line comment.
+
+--Zero-P Version 1.0.1 ()--
---
+++ index.js
@@ -0,0 +1,9 @@
+// Hi ! If you are reading this comment I need your help . Can you add a command that makes my items appear with a appear animation like show(1000) when $('document').ready ?
+
+// Ready by default containers
+
+$('document').ready(function() {
+    $('.hero-cover').show();
+    $('.whole-nav').show();
+});
+
