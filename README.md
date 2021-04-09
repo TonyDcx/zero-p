fcbb6fe0713212f3b12316c6c60435f57a3b14da
TonDcx <official.tony.one@gmail.com>
2021-04-09 12:21:53 +0300

    1 1.0.1
    Removed Jquery - Added Plain Javascript
    
    Added Functions and Transition
    
    See To Do List

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # zero-p
 
---Zero-P Version 1.0.0 (9.04.2021)--
+--Zero-P Version 1.0.0 (8.04.2021)--
 
 Added the files .
 
@@ -10,4 +10,10 @@ Added some design / font / variables and rules / infos for variables .
 
 Don't forget to check my js file and see the multi-line comment.
 
---Zero-P Version 1.0.1 ()--
+--Zero-P Version 1.0.1 (9.04.2021)--
+
+Changed JQuery In JavaScript
+
+Added animations on transitions
+
+Added functions with Javascript
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-// Hi ! If you are reading this comment I need your help . Can you add a command that makes my items appear with a appear animation like show(1000) when $('document').ready ?
+/* 
+Hi ! If you are reading this comment I need your help . 
+Can you add a command that makes my items appear with a appear animation like show(1000) when $('document').ready ?
+*/
 
 // Ready by default containers
 
-$('document').ready(function() {
-    $('.hero-cover').show();
-    $('.whole-nav').show();
-});
+var hideHero = document.getElementsByClassName('hero-cover')[0];
 
+hideHero.style.display = "none";
+
+// Some variables 
+
+var showHero = document.getElementsByClassName('hero-cover')[0];
+
+function pressShowCover() {
+    showHero.style.display = "block";
+}
+
+function pressHideCover() {
+    hideHero.style.display = "none";
+}
