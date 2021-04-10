b7701accfaf0f1099f84e2ddec61dea19a95d919
TonDcx <official.tony.one@gmail.com>
2021-04-11 00:12:43 +0300

    New Design

--- js/index.js
+++ js/index.js
@@ -4,14 +4,16 @@ const swipeAction = document.getElementsByClassName('swipe-full')[0];
 
 const textBox = document.getElementsByClassName('text-box-full')[0];
 
+const closeButton = document.getElementsByClassName('close')[0];
+
 // By Default //
 
-textBox.style.display = "none";
+textBox.style.display = "flex";
 
 // Basic tasks //
 
-function textBoxShow() {
-    textBox.style.display = "flex";
+function closeBox() {
+    textBox.style.display = "none";
 }
 
 // Console Log //
